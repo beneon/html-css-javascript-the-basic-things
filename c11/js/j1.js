50ff02e580090a818022218717439b1425d23542
beneon <lucidstudio@gmail.com>
2016-05-08 15:09:30 +0800

    c11 preparation begin

---
+++ c11/js/j1.js
@@ -0,0 +1,8 @@
+function colorChange(){
+  document.getElementById("colorBox").style.backgroundColor = "#fff";
+  document.getElementById("colorBox").style.border = "solid 1px";
+}
+
+function colorChangePara(color){
+  document.getElementById("colorBox").style.backgroundColor = color
+}
