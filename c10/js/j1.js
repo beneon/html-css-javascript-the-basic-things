05da3f2f867f509ab14965218ececa90e6d5afd9
beneon <lucidstudio@gmail.com>
2017-02-27 11:07:51 +0800

    2017年上半年课程内容

--- c10/js/j1.js
+++
@@ -1,8 +0,0 @@
-function colorChange(){
-  document.getElementById("colorBox").style.backgroundColor = "#fff";
-  document.getElementById("colorBox").style.border = "solid 1px";
-}
-
-function colorChangePara(color){
-  document.getElementById("colorBox").style.backgroundColor = color
-}
