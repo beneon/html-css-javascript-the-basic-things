fe69963af288afc28fea4d8fd1403054c56151fd
beneon <lucidstudio@gmail.com>
2017-03-27 23:46:42 +0800

    第五次课前

--- test.js
+++
@@ -1,5 +0,0 @@
-var name = ["Lorem","ipsum","dolor","sit","amet","consectetur","adipisicing","elit","sed","do","eiusmod","tempor","iNcididunt","ut","labore","et","dolore","magna","Ut","enim","ad","minim","veniam","quis"]
-for (var i = 0; i < name.length; i++) {
-	name[i] = name[i].toUpperCase()
-}
-console.log(name);
